1f4970b9141f4abfb04a6c9ca4add6e87e499bec
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-08-20 09:00:51 +0700

    update: assets | dev: components, constants

--- src/constants/customStyles.js
+++ src/constants/customStyles.js
@@ -2,11 +2,7 @@ import { CUSTOM_COLOR } from './colors';
 
 export const Shadow = {
   shadowColor: CUSTOM_COLOR.black,
-  shadowOffset: {
-    width: 0,
-    height: 2,
-  },
-  shadowOpacity: 0.2,
-  shadowRadius: 8,
-  elevation: 4,
+  shadowOpacity: 0.05,
+  shadowRadius: 80,
+  elevation: 0.5,
 };
