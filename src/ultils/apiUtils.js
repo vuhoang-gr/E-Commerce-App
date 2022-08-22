b31037a3a1e1a156d76f268d581d6beaa735c22a
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-08-22 21:30:11 +0700

    update: api config

--- src/ultils/apiUtils.js
+++ src/ultils/apiUtils.js
@@ -1,13 +1,18 @@
 import axios from 'axios'
 import queryString from 'query-string'
 import auth from '@react-native-firebase/auth'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const getFirebaseToken = async () => {
     const currentUser = auth().currentUser;
-    if(currentUser) return currentUser.getIdToken();
+    if (currentUser) {
+        const jwt = await currentUser.getIdToken(true);
+        // console.log('compare: ', jwt === tokenStorage);
+        return jwt
+    }
 
-    const rememberAccount = localStorage.getItem('firebaseui::rememberedAccounts');
-    if(!rememberAccount) return null;
+    const tokenStorage = await AsyncStorage.getItem('ACCESS_TOKEN');
+    if (!tokenStorage) return null;
 
     return new Promise((resolve, reject) => {
         const waiter = setTimeout(() => {
@@ -15,29 +20,50 @@ const getFirebaseToken = async () => {
         }, 5000);
         const subcriber = auth().onAuthStateChanged(async (user) => {
             if (!user) {
-              reject(null);
+                reject(null);
             }
-            const token = await user.getIdToken();
-            // console.log('[Axios] Logged in user token: ', token);
+            const token = await user.getIdToken(true);
             resolve(token);
 
             subcriber();
             clearTimeout(waiter);
-          });
+        });
     })
 }
 
-const axiosClient = axios.create({
-    baseURL: 'myURL',
+const filterStringtify = (filter) => {
+    const res = '';
+    if (filter)
+        res = 'orderBy="' + filter.key + '"&';
+    else return null;
+    if (filter.type === 'equal')
+        res += `equalTo=${filter.data}`;
+    else if (filter.type === 'start')
+        res += `startAt=${filter.data}`;
+    else if (filter.type === 'end')
+        res += `endAt=${filter.data}`;
+    else return null;
+    res += '&';
+    return String(res);
+}
+
+const REQUEST_TIMEOUT = 30000;
+
+export const axiosClient = axios.create({
+    // baseURL: 'myURL',
+    baseURL: 'https://e-commerce-79103-default-rtdb.firebaseio.com/',
     headers: {
         'content-type': 'application/json',
     },
     paramsSerializer: params => queryString.stringify(params),
 });
 
+axiosClient.defaults.withCredentials = true;
+
 axiosClient.interceptors.request.use(async (config) => {
     const token = await getFirebaseToken();
-    if(token) {
+    // console.log('request token: ', token);
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
@@ -47,13 +73,63 @@ axiosClient.interceptors.request.use(async (config) => {
 })
 
 axiosClient.interceptors.response.use((response) => {
-    if(response && response.data) {
-        return response.data;
-    }
+    // if (response && response.data) {
+    //     return response.data;
+    // }
     return response;
 }, (error) => {
     //handle err
     console.log(error);
 })
 
-export default axiosClient;
+const APIUtils = {
+    get: async (url, filter, headers) => {
+        const flt = filterStringtify(filter);
+        const token = await getFirebaseToken();
+        const rsp = axiosClient.get(`${url}.json?${flt ? flt : ''}auth=${token}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'text/plain',
+                ...headers,
+            },
+            timeout: REQUEST_TIMEOUT
+        })
+            .catch(e => console.log('get error', e));
+        return rsp;
+    },
+    put: async (url, data, headers) => {
+        const token = await getFirebaseToken();
+        const rsp = axiosClient.put(`${url}.json?auth=${token}`, data, {
+            timeout: REQUEST_TIMEOUT,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
+            }
+        }).catch(e => console.log('put error', e))
+        return rsp;
+    },
+    delete: async (url, body, headers) => {
+        const token = await getFirebaseToken();
+        const rsp = axiosClient.delete(`${url}.json?auth=${token}`, {
+            timeout: REQUEST_TIMEOUT,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
+            }
+        }).catch(e => console.log('del error', e));
+        return rsp;
+    },
+    patch: async (url, data, headers) => {
+        const token = await getFirebaseToken();
+        const rsp = axiosClient.patch(`${url}.json?auth=${token}`, data, {
+            timeout: REQUEST_TIMEOUT,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
+            }
+        }).catch(e => console.log('patch error', e))
+        return rsp;
+    }
+}
+
+export default APIUtils;
